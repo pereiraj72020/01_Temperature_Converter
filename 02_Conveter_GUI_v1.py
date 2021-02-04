4efa67ee2c821c2637c17dcfb98f5d38e6e48b3e
pereiraj72020 <51728484+pereiraj72020@users.noreply.github.com>
2021-02-05 11:20:56 +1300

    Create 02_Conveter_GUI_v1.py

---
+++ 02_Conveter_GUI_v1.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from functools import partial   # To prevent unwanted windows
+import random
+
+
+class Converter:
+    def __init__(self):
+
+        # Formatting variables
+        background_color = "light blue"
+
+        # Converter Frame
+        self.converter_frame = Frame(width=300, bg=background_color,
+                                     pady=10)
+        self.converter_frame.grid()
+
+        # Temperature Converter Heading (row 0)
+        self.temp_heading_label = Label(self.converter_frame,
+                                        text="Temperature Converter",
+                                        font="Arial 16 bold",
+                                        bg=background_color,
+                                        padx=10, pady=10)
+        self.temp_heading_label.grid(row=0)
+
+        # User instructions (row 1)
+        self.temp_instructions_label = Label(self.converter_frame,
+                                             text= "Type in the amount to be "
+                                                   "Converted and then push "
+                                                   "one of the buttons below...",
+                                             font="Arial 10 italic", wrap=250,
+                                             justify=LEFT, bg=background_color)
+        # Temperature entry box (row 1)
+
+        # Conversion buttons frame (row 1)
+
+        # Answer label (row 4)
+
+        # History / Help button frame (row 5)
